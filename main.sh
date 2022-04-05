27d999671b4aab576fa7287baa22d187e092125e
112c <tobyfox2020@gmail.com>
2022-04-05 08:50:16 +0000

    main

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
-apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session"'
-sudo systemctl disable lightdm.service
+exec xfce4-session
